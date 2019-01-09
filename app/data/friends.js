7b6de3529d7eb1f976e79e2759f760277a53e47e
ogammal <gammal93@mac.com>
2019-01-09 18:54:55 -0500

    api friends list

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,276 @@
 var friendsArray = [
     {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
-    }      
-]
+    "name": "Ahmed",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    "scores": [
+    "5",
+    "1",
+    "4",
+    "4",
+    "5",
+    "1",
+    "2",
+    "5",
+    "4",
+    "1"
+    ]
+    },
+    {
+    "name": "Jacob Deming",
+    "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    "scores": [
+    "4",
+    "2",
+    "5",
+    "1",
+    "3",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2"
+    ]
+    },
+    {
+    "name": "Jeremiah Scanlon",
+    "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    "scores": [
+    "5",
+    "2",
+    "2",
+    "2",
+    "4",
+    "1",
+    "3",
+    "2",
+    "5",
+    "5"
+    ]
+    },
+    {
+    "name": "Louis T. Delia",
+    "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+    "scores": [
+    "3",
+    "3",
+    "4",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2",
+    "2",
+    "3"
+    ]
+    },
+    {
+    "name": "Lou Ritter",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+    "scores": [
+    "4",
+    "3",
+    "4",
+    "1",
+    "5",
+    "2",
+    "5",
+    "3",
+    "1",
+    "4"
+    ]
+    },
+    {
+    "name": "Jordan Biason",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+    "scores": [
+    "4",
+    "4",
+    "2",
+    "3",
+    "2",
+    "2",
+    "3",
+    "2",
+    "4",
+    "5"
+    ]
+    },
+    {
+    "name": "Friendly Frank",
+    "photo": "pic",
+    "scores": [
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1"
+    ]
+    },
+    {
+    "name": "Friendly Frank",
+    "photo": "pic",
+    "scores": [
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1"
+    ]
+    },
+    {
+    "name": "Friendly Frank",
+    "photo": "pic",
+    "scores": [
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1"
+    ]
+    },
+    {
+    "name": "e",
+    "photo": "e",
+    "scores": [
+    "2",
+    "2",
+    "3",
+    "2",
+    "2",
+    "3",
+    "3",
+    "2",
+    "2",
+    "1"
+    ]
+    },
+    {
+    "name": "e",
+    "photo": "e",
+    "scores": [
+    "2",
+    "2",
+    "3",
+    "2",
+    "2",
+    "3",
+    "3",
+    "2",
+    "2",
+    "1"
+    ]
+    },
+    {
+    "name": "ds",
+    "photo": "sd",
+    "scores": [
+    "3",
+    "3",
+    "4",
+    "3",
+    "2",
+    "3",
+    "3",
+    "4",
+    "3",
+    "3"
+    ]
+    },
+    {
+    "name": "Test",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwji3JWrwOHfAhUPGTQIHW2RAu4QjRx6BAgBEAU&url=http%3A%2F%2F24eastmain.com%2Ftest%2F&psig=AOvVaw2_D_AHkrrD6c3OdDbbDJU1&ust=1547150884721638",
+    "scores": [
+    "5",
+    "3",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2"
+    ]
+    },
+    {
+    "name": "Test",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwji3JWrwOHfAhUPGTQIHW2RAu4QjRx6BAgBEAU&url=http%3A%2F%2F24eastmain.com%2Ftest%2F&psig=AOvVaw2_D_AHkrrD6c3OdDbbDJU1&ust=1547150884721638",
+    "scores": [
+    "5",
+    "3",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2"
+    ]
+    },
+    {
+    "name": "Test",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwji3JWrwOHfAhUPGTQIHW2RAu4QjRx6BAgBEAU&url=http%3A%2F%2F24eastmain.com%2Ftest%2F&psig=AOvVaw2_D_AHkrrD6c3OdDbbDJU1&ust=1547150884721638",
+    "scores": [
+    "5",
+    "3",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2"
+    ]
+    },
+    {
+    "name": "efdef",
+    "photo": "ffdsdsf",
+    "scores": [
+    "2",
+    "4",
+    "4",
+    "1",
+    "1",
+    "2",
+    "5",
+    "5",
+    "5",
+    "5"
+    ]
+    },
+    {
+    "name": "mil",
+    "photo": "m,m,",
+    "scores": [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5"
+    ]
+    }
+    ]
+
+    module.exports = friendsArray;
